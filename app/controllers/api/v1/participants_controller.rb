062f8eaaaf82d426b3163d7eec9b6ceebbea17ed
Vadim Alexandra <2018topdev531@gmail.com>
2019-06-20 13:49:45 +1000

    update on PR review

--- app/controllers/api/v1/participants_controller.rb
+++ app/controllers/api/v1/participants_controller.rb
@@ -1,5 +1,6 @@
 module Api::V1
   class ParticipantsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :not_found
 
     def index
       render json: Participant.accepted
@@ -16,12 +17,9 @@ module Api::V1
     end
 
     def update
-      unless Participant.statuses.keys.include?(params[:status])
-        render json: { error: 'Invalid status' }
-        return
-      end
+      return invalid_status unless status_param_valid?
 
-      if participant = Participant.find_by_id(params[:id])
+      if participant = Participant.find(params[:id])
         if participant.pending?
           participant.status = params[:status]
           participant.save
@@ -40,5 +38,16 @@ module Api::V1
       params.permit(:f_name, :l_name, :phone, :email, :gender, :current_weight, :height, :zip)
     end
 
+    def invalid_status
+      render json: { message: "Invalid status" }
+    end
+
+    def status_param_valid?
+      Participant.statuses.keys.include?(params[:status])
+    end
+
+    def not_found
+      render json: { message: "Not Found" }, status: 404
+    end
   end
 end
